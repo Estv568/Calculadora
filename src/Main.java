737c49d0b29777cdf624577172098242e07668dc
Estv568 <eltororo322@gmail.com>
2023-05-07 00:52:09 -0300

    multiplicação

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,14 @@ public class Main {
         int numero = 0;
         Scanner tcd = new Scanner(System.in);
 
+        ///entrada de processamento
         System.out.println("Digite um numero");
         numero = tcd.nextInt();
 
         for (int n = 1; n<11 ; n++){
 
             r = numero * n;
-
+             ///saida
             System.out.println( numero+"x"+n+"=" + r );
         }
 
